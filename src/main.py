68f6a5ef59d9c3b569d100f0aba59cd79e811018
Nikita Fomin <defdXs@yandex.ru>
2025-09-26 17:42:31 +0300

    base kaef

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import logging
 import sys, os
+from datetime import datetime
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s]: %(message)s",
     handlers=[
         logging.StreamHandler(sys.stdout),
-        logging.FileHandler("log/app.log", encoding="utf-8")
+        logging.FileHandler(f"log/{datetime.now()}.log", encoding="utf-8")
     ]
 )
 
