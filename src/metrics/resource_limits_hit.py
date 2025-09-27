191dc350e6f90df3907e0a7afbf192c73e89fbb4
Nikita Fomin <defdXs@yandex.ru>
2025-09-27 11:41:07 +0300

    base fix

--- src/metrics/resource_limits_hit.py
+++ src/metrics/resource_limits_hit.py
@@ -3,12 +3,14 @@ from logging import info, warning, error
 
 def analyze_metrics(metrics):
     limits = metrics.get('limits', {})
-    if type(limits['Max processes']['soft']) is str:
+    try:    
+        if 'Max processes' in limits and int(limits['Max processes']['soft']) < 1024:
+            warning("Проблема: Достигнуты лимиты ресурсов обнаружены. Рекомендации: Увеличьте лимиты в /etc/security/limits.conf (например, * soft nproc 65535). Примените: ulimit -u 65535 или prlimit --pid <pid> --nofile=65535:65535. Проверьте: cat /proc/<pid>/limits.")
+        else:
+            info("Проблема: Достигнуты лимиты ресурсов не обнаружены.")
+    except:
         warning(f"{limits['Max processes']['soft']} Проблема: Процесс не имеет лимита. Рекомендации: Установите лимиты в /etc/security/limits.conf (например, * soft nproc 65535). Примените: ulimit -u 65535 или prlimit --pid <pid> --nofile=65535:65535. Проверьте: cat /proc/<pid>/limits.")
-    elif 'Max processes' in limits and int(limits['Max processes']['soft']) < 1024:
-        warning("Проблема: Достигнуты лимиты ресурсов обнаружены. Рекомендации: Увеличьте лимиты в /etc/security/limits.conf (например, * soft nproc 65535). Примените: ulimit -u 65535 или prlimit --pid <pid> --nofile=65535:65535. Проверьте: cat /proc/<pid>/limits.")
-    else:
-        info("Проблема: Достигнуты лимиты ресурсов не обнаружены.")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
