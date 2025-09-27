156f3331482513958f56c5305e6c8a0fdf804123
Nikita Fomin <defdXs@yandex.ru>
2025-09-27 10:10:46 +0300

    base autocheck

--- src/main.py
+++ src/main.py
@@ -1,21 +1,8 @@
 import logging
 import sys, os
-from metrics import process_metrics, \
-    fd_leak, \
-    high_cpu_usage, \
-    high_page_faults, \
-    io_bottleneck, \
-    library_memory_usage, \
-    memory_leak_detection, \
-    memory_overcommit, \
-    priority_inversion, \
-    resource_limits_hit, \
-    scheduling_latency, \
-    signal_overload, \
-    stack_overflow_risk, \
-    thread_concurrency_issue, \
-    zombie_processes
 from datetime import datetime
+import pkgutil
+import importlib
 
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 log_filename = f"log/{timestamp}.log"
@@ -30,22 +17,27 @@ logging.basicConfig(
 )
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <pid>")
+        sys.exit(1)
+    
     pid = sys.argv[1]
-    metrics = process_metrics.get_process_metrics(pid)
-    for metric in [
-        fd_leak,
-        high_cpu_usage,
-        high_page_faults,
-        io_bottleneck,
-        library_memory_usage,
-        memory_leak_detection,
-        memory_overcommit,
-        priority_inversion,
-        # resource_limits_hit,
-        scheduling_latency,
-        signal_overload,
-        stack_overflow_risk,
-        thread_concurrency_issue,
-        zombie_processes
-    ]:
-        metric.analyze_metrics(metrics)
+    
+    import metrics
+    from metrics import process_metrics
+    data = process_metrics.get_process_metrics(pid)
+    
+    for importer, modname, ispkg in pkgutil.iter_modules(metrics.__path__, metrics.__name__ + "."):
+        if modname.endswith(".process_metrics"):
+            continue
+
+        try:
+            module = importlib.import_module(modname)
+            
+            if hasattr(module, "analyze_metrics"):
+                logging.info(f"Running analysis from {modname}")
+                module.analyze_metrics(data)
+            else:
+                logging.debug(f"Module {modname} has no 'analyze_metrics' function — skipping")
+        except Exception as e:
+            logging.error(f"Failed to import or run {modname}: {e}")
